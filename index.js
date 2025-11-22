f3af00a2db3121b247e1295036ae5c3e815e012e
Thankgod Ode <thankgodode535@gmail.com>
2025-11-22 13:34:28 +0100

    Fixed some CORS issue

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const bodyParser = require("body-parser")
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ origin: "http://localhost:8081", credentials: true }));
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow mobile apps (origin = null)
+    if (!origin) return callback(null, true);
+
+    // Allow browser dev environment
+    if (origin.startsWith("http://localhost")) return callback(null, true);
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true
+}));
+
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json({ limit: "5mb" }))
