1aec0df35a1715407d648d6a23af7a1529d36c96
Thankgod Ode <thankgodode535@gmail.com>
2025-11-22 13:52:21 +0100

    Another commit

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.use(express.json({ limit: "5mb" }))
 
 app.use("/api/toDocx", require("./routes/docx"))
 
+app.get("/", (req, res) =>{
+    res.status(200).json({message:"Backend working...!"})
+})
+
 app.listen(PORT, () => {
     console.log("Routing on Port: ", PORT)
 });
